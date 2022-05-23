458aa95fb5234989a91775d47d986f745e80f6fa
ElginL <elginlee_2000@hotmail.com>
2022-05-23 14:12:26 +0800

    add login logout frontend

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/Login';
+import Signup from './src/screens/Signup';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/screens/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';   
+
+const Login = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const signInHandler = () => {
+        console.log("Some backend stuff goes here");
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.screenContainer}>
+                <Text style={styles.headerText}>Log In Page</Text>
+                <View style={styles.formContainer}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={input => setEmail(email + input)}
+                        keyboardType="email-address"
+                        placeholder="Enter your email address"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={input => setPassword(password + input)}
+                        placeholder="Enter your password"
+                        secureTextEntry
+                    />
+                    <Button
+                        title="Sign In"
+                        onPress={signInHandler}
+                    />
+                    <Button
+                        title="Don't have an account? Click here to Sign Up"
+                        onPress={() => navigation.navigate("Signup")}
+                    />
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1
+    },
+    headerText: {
+        fontSize: 40,
+        textAlign: 'center',
+        marginTop: 60,
+        marginBottom: 30
+    },
+    input: {
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        width: '70%',
+        margin: 10
+    },
+    formContainer: {
+        alignItems: 'center'
+    }
+});
+
+export default Login;
